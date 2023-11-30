0ebde841a45e6df5c14f5cf6fb92b9767a3c1ed2
Mischlichter <83180931+Mischlichter@users.noreply.github.com>
2023-11-30 02:04:15 +0100

    Update scale_images.py

--- gallery/scaled/scale_images.py
+++ gallery/scaled/scale_images.py
@@ -7,26 +7,30 @@ output_folder = 'gallery/scaled/'
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
+# Define the scaling factor (123.6%)
+scaling_factor = 1.236
+
 for filename in os.listdir(input_folder):
     if filename.endswith('.jpg') or filename.endswith('.png'):
         print(f"Processing file: {filename}")
         try:
             with Image.open(os.path.join(input_folder, filename)) as img:
-                # Calculate new dimensions
-                new_width = int(img.width * 1.236)  # Scale up to 123.6%
-                new_height = int(img.height * 1.236)
+                # Calculate the new dimensions based on the scaling factor
+                new_width = int(img.width * scaling_factor)
+                new_height = int(img.height * scaling_factor)
 
-                # Crop the center to maintain original dimensions
-                left = (new_width - img.width) // 2
-                top = (new_height - img.height) // 2
-                right = (new_width + img.width) // 2
-                bottom = (new_height + img.height) // 2
-
+                # Resize the image while keeping the original dimensions
                 img = img.resize((new_width, new_height), Image.LANCZOS)
-                img = img.crop((left, top, right, bottom))
+
+                # Calculate the crop dimensions based on the golden ratio
+                crop_top = int((new_height - img.height) * 0.382)  # 38.2% from the top
+                crop_bottom = new_height - crop_top
+
+                # Crop the image
+                img = img.crop((0, crop_top, img.width, crop_bottom))
 
                 new_filename = os.path.splitext(filename)[0] + '_scaled' + os.path.splitext(filename)[1]
                 img.save(os.path.join(output_folder, new_filename))
-                print(f"Saved scaled file: {new_filename}")
+                print(f"Saved scaled and cropped file: {new_filename}")
         except Exception as e:
             print(f"Error processing {filename}: {e}")
