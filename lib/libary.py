f933d3b469f5fc18f8203771ec0900b33ff0afd8
Mischlichter <83180931+Mischlichter@users.noreply.github.com>
2023-12-04 02:38:16 +0100

    123

--- lib/libary.py
+++ lib/libary.py
@@ -15,10 +15,13 @@ def update_metadata_json(metadata, json_file):
     with open(json_file, 'w') as file:
         json.dump(metadata, file, indent=4)
 
+
 def main():
     base_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
     gallery_path = os.path.join(base_path, 'gallerycom')
-    json_file_path = os.path.join(base_path, 'lib/metadata.json')
+    # Inside libary.py
+	json_file_path = os.path.join(base_path, 'lib/metadata.json')
+
 
     # Load existing metadata
     if os.path.exists(json_file_path):
