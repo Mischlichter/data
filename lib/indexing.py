b586a32485952c8ec0dbe9be6c8187c3c7b095f7
Mischlichter <83180931+Mischlichter@users.noreply.github.com>
2024-04-17 16:53:41 +0200

    Update indexing.py

--- lib/indexing.py
+++ lib/indexing.py
@@ -6,13 +6,14 @@ from datetime import datetime
 def get_file_info():
     all_files = {}
     for path in Path('.').rglob('*'):
-        if path.is_file() and not path.parts[0] in ['docs', 'gallerycom', 'lib']:
+        if path.is_file():
             all_files[str(path)] = datetime.utcfromtimestamp(path.stat().st_mtime).strftime('%Y-%m-%d %H:%M:%S')
     return all_files
 
 def main():
     files_info = get_file_info()
-    with open('lib/index.json', 'w') as json_file:  # Save in the lib folder
+    os.makedirs('lib', exist_ok=True)  # Ensure the 'lib' directory exists
+    with open('lib/index.json', 'w') as json_file:  # Save in the 'lib' folder
         json.dump(files_info, json_file, indent=2)
 
 if __name__ == "__main__":
