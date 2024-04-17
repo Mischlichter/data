314f18729c302d853a3f7a75832a96a6a016aeec
Mischlichter <83180931+Mischlichter@users.noreply.github.com>
2024-04-17 17:08:08 +0200

    Update indexing.py

--- lib/indexing.py
+++ lib/indexing.py
@@ -7,12 +7,15 @@ def get_file_info():
     all_files = {}
     for path in Path('.').rglob('*'):
         if path.is_file():
-            all_files[str(path)] = datetime.fromtimestamp(path.stat().st_mtime, timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
+            # Ensures the file path is relative to the current directory
+            rel_path = os.path.relpath(path, start=Path('.'))
+            all_files[rel_path] = datetime.fromtimestamp(path.stat().st_mtime, timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
     return all_files
 
 def main():
     files_info = get_file_info()
-    with open('index.json', 'w') as json_file:  # Note: 'lib/' is removed
+    # Directly write to 'index.json' assuming the current directory is correct
+    with open('index.json', 'w') as json_file:
         json.dump(files_info, json_file, indent=2)
 
 if __name__ == "__main__":
