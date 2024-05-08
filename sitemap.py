88660c28dfdd5b5a3a92b7dfba17da985bbf1b21
Mischlichter <83180931+Mischlichter@users.noreply.github.com>
2024-05-09 00:20:14 +0200

    564465

--- sitemap.py
+++ sitemap.py
@@ -5,7 +5,7 @@ from urllib.parse import quote
 # Configuration
 base_url = "https://www.hogeai.com/"  # Adjusted base URL of your site
 docs_path = "./docs"  # Local path to the 'docs' directory relative to where the script is run
-sitemap_path = "./docs/sitemap.xml"  # Path to save the sitemap
+sitemap_path = "./sitemap.xml"  # Path to save the sitemap
 
 def generate_sitemap(directory, base_url):
     urlset = ET.Element("urlset", xmlns="http://www.sitemaps.org/schemas/sitemap/0.9")
