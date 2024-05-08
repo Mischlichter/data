9e5a726a40851d0f9979be36bbf896cd8ac5b3e3
Mischlichter <83180931+Mischlichter@users.noreply.github.com>
2024-05-09 01:51:43 +0200

    123

--- lib/sitemap.py
+++ lib/sitemap.py
@@ -28,22 +28,7 @@ def generate_sitemap(directory, base_url, sitemap_path):
 
     tree = ET.ElementTree(urlset)
     tree.write(sitemap_path, xml_declaration=True, encoding='utf-8', method="xml")
+    print(f"Sitemap generated at {sitemap_path}")
 
 if __name__ == "__main__":
-    # Check if the sitemap file exists
-    if not os.path.exists(sitemap_path):
-        print("Sitemap file does not exist. Generating a new one.")
-        generate_sitemap(docs_path, base_url, sitemap_path)
-    else:
-        # Check if any HTML files have been modified since the last sitemap generation
-        last_sitemap_modification = get_last_modified(sitemap_path)
-        for subdir, dirs, files in os.walk(docs_path):
-            for file in files:
-                if file.endswith(".html"):
-                    file_path = os.path.join(subdir, file)
-                    if get_last_modified(file_path) > last_sitemap_modification:
-                        print(f"Detected changes in '{file_path}'. Updating sitemap.")
-                        generate_sitemap(docs_path, base_url, sitemap_path)
-                        break
-        else:
-            print("No changes detected. Sitemap is up to date.")
+    generate_sitemap(docs_path, base_url, sitemap_path)
