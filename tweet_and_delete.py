b6a5412e8e1e79ca3b32abd5a1fe12b4b5670e61
Mischlichter <83180931+Mischlichter@users.noreply.github.com>
2024-05-09 17:44:29 +0200

    342

--- tweet_and_delete.py
+++ tweet_and_delete.py
@@ -1,42 +1,43 @@
 import tweepy
 import os
-
+import time
 import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
 def create_api():
+    # Fetch credentials directly from environment variables
     consumer_key = os.getenv('TWITTER_API_KEY')
     consumer_secret = os.getenv('TWITTER_API_SECRET')
-    access_token = os.getenv('TWITTER_ACCESS_TOKEN').replace('_', '-')
-    access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET').replace('_', '-')
+    access_token = os.getenv('TWITTER_ACCESS_TOKEN')
+    access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET')
     
-    logging.debug(f"Consumer Key: {consumer_key}")
-    logging.debug(f"Consumer Secret: {consumer_secret}")
-    logging.debug(f"Access Token: {access_token}")
-    logging.debug(f"Access Token Secret: {access_token_secret}")
+
     
+    # Setup Tweepy API authentication
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     
     api = tweepy.API(auth)
     return api
 
-
 def tweet_and_delete(api):
     try:
+        # Read HTML filenames from a text file
         with open('html_files.txt', 'r') as file:
             html_files = file.readlines()
         
+        # Tweet each URL and then delete it after 60 seconds
         for html_file in html_files:
             filename = html_file.strip()
             url = f"https://www.hogeai.com/sharing/{filename}"
-            tweet_content = f"{url}"
+            tweet_content = f"Check out our latest content: {url}"
             
+            # Post a tweet
             tweet = api.update_status(tweet_content)
             print(f"Tweeted: {tweet.id}")
 
-            # Wait for 10 seconds
+            # Wait for 60 seconds
             time.sleep(60)
 
             # Delete the tweet
