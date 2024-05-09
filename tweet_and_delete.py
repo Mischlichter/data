f3d0283ee00417bbb80fde3947118695c78bbd5c
Mischlichter <83180931+Mischlichter@users.noreply.github.com>
2024-05-09 06:51:17 +0200

    342

--- tweet_and_delete.py
+++ tweet_and_delete.py
@@ -3,19 +3,23 @@ import time
 import os
 
 def create_api():
-    consumer_key = os.environ['TWITTER_API_KEY']
-    consumer_secret = os.environ['TWITTER_API_SECRET']
-    access_token = os.environ['TWITTER_ACCESS_TOKEN']
-    access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+    # Fetch environment variables and replace underscores with dashes if needed
+    consumer_key = os.getenv('TWITTER_API_KEY').replace('_', '-')
+    consumer_secret = os.getenv('TWITTER_API_SECRET').replace('_', '-')
+    access_token = os.getenv('TWITTER_ACCESS_TOKEN').replace('_', '-')
+    access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET').replace('_', '-')
     
+    # Authenticate with Twitter using Tweepy
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     
+    # Create the API object
     api = tweepy.API(auth)
     return api
 
 def tweet_and_delete(api):
     try:
+        # Read the HTML files list
         with open('html_files.txt', 'r') as file:
             html_files = file.readlines()
         
@@ -28,8 +32,8 @@ def tweet_and_delete(api):
             tweet = api.update_status(tweet_content)
             print(f"Tweeted: {tweet.id}")
 
-            # Wait for 10 seconds
-            time.sleep(11)
+            # Wait for 10 seconds before deleting the tweet
+            time.sleep(30)
 
             # Delete the tweet
             api.destroy_status(tweet.id)
