c2ab8fec2e10e5651889b284951a033bda5298b1
Mischlichter <83180931+Mischlichter@users.noreply.github.com>
2024-05-09 07:04:28 +0200

    342

--- tweet_and_delete.py
+++ tweet_and_delete.py
@@ -1,39 +1,34 @@
 import tweepy
-import time
 import os
 
 def create_api():
-    # Fetch environment variables and replace underscores with dashes if needed
-    consumer_key = os.getenv('TWITTER_API_KEY').replace('_', '-')
-    consumer_secret = os.getenv('TWITTER_API_SECRET').replace('_', '-')
+    consumer_key = os.getenv('TWITTER_API_KEY')
+    consumer_secret = os.getenv('TWITTER_API_SECRET')
+    # Correcting the token format from underscore to dash
     access_token = os.getenv('TWITTER_ACCESS_TOKEN').replace('_', '-')
     access_token_secret = os.getenv('TWITTER_ACCESS_TOKEN_SECRET').replace('_', '-')
     
-    # Authenticate with Twitter using Tweepy
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     
-    # Create the API object
     api = tweepy.API(auth)
     return api
 
 def tweet_and_delete(api):
     try:
-        # Read the HTML files list
         with open('html_files.txt', 'r') as file:
             html_files = file.readlines()
         
         for html_file in html_files:
             filename = html_file.strip()
             url = f"https://www.hogeai.com/sharing/{filename}"
-            tweet_content = f"Check out our new content: {url}"
+            tweet_content = f"{url}"
             
-            # Tweet the content
             tweet = api.update_status(tweet_content)
             print(f"Tweeted: {tweet.id}")
 
-            # Wait for 10 seconds before deleting the tweet
-            time.sleep(30)
+            # Wait for 10 seconds
+            time.sleep(10)
 
             # Delete the tweet
             api.destroy_status(tweet.id)
